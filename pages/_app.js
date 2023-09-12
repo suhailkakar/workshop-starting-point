3c0275bcaef758d93dd0e503e955d8a4e34eead3
Suhail Kakar <suhail.zahed@gmail.com>
2023-09-12 19:49:47 +0530

    add wagmi

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,39 @@
 import client from "@/client/livepeer";
 import "@/styles/globals.css";
 import { LivepeerConfig } from "@livepeer/react";
+import "@rainbow-me/rainbowkit/styles.css";
+
+import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { configureChains, createConfig, WagmiConfig } from "wagmi";
+import { polygonMumbai } from "wagmi/chains";
+import { alchemyProvider } from "wagmi/providers/alchemy";
+import { publicProvider } from "wagmi/providers/public";
 
 export default function App({ Component, pageProps }) {
+  const { chains, publicClient } = configureChains(
+    [polygonMumbai],
+    [publicProvider()]
+  );
+
+  const { connectors } = getDefaultWallets({
+    appName: "My Livepeer App",
+    projectId: "YOUR_PROJECT_ID",
+    chains,
+  });
+
+  const wagmiConfig = createConfig({
+    autoConnect: true,
+    connectors,
+    publicClient,
+  });
+
   return (
-    <LivepeerConfig client={client}>
-      <Component {...pageProps} />
-    </LivepeerConfig>
+    <WagmiConfig config={wagmiConfig}>
+      <RainbowKitProvider chains={chains}>
+        <LivepeerConfig client={client}>
+          <Component {...pageProps} />
+        </LivepeerConfig>
+      </RainbowKitProvider>
+    </WagmiConfig>
   );
 }
